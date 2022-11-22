9bbec0d0db6fc7ff99e66ae988a4150c88d9f490
Felice Scala <scala.felice@hotmail.it>
2022-11-22 10:45:22 +0100

    Created Tool Interface

---
+++ src/it/unisa/diem/se2022/drawingapp/group11IZ/models/MyEnhancedLine.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.unisa.diem.se2022.drawingapp.group11IZ.models;
+
+import javafx.scene.shape.Line;
+
+/**
+ *
+ * @author utente
+ */
+public class MyEnhancedLine extends Line implements MyLine{
+    
+}
---
+++ src/it/unisa/diem/se2022/drawingapp/group11IZ/models/MyEnhancedRectangle.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.unisa.diem.se2022.drawingapp.group11IZ.models;
+
+import javafx.scene.shape.Rectangle;
+
+/**
+ *
+ * @author utente
+ */
+public class MyEnhancedRectangle extends Rectangle implements MyRectangle{
+    
+}
